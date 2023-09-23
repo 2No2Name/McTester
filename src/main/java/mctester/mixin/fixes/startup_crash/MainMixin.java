f1e90374b095804f2b573eb7866482ce9fdc89df
2No2Name <2No2Name@web.de>
2023-09-23 08:21:45 +0200

    Crash on failed startup

---
+++ src/main/java/mctester/mixin/fixes/startup_crash/MainMixin.java
@@ -0,0 +1,19 @@
+package mctester.mixin.fixes.startup_crash;
+
+import net.minecraft.server.Main;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Main.class)
+public class MainMixin {
+
+    @Inject(
+            method = "main", remap = false,
+            at = @At(value = "INVOKE", target = "Lorg/slf4j/Logger;error(Lorg/slf4j/Marker;Ljava/lang/String;Ljava/lang/Throwable;)V")
+    )
+    private static void crashServerStartup(String[] args, CallbackInfo ci) {
+        throw new IllegalStateException("Server startup failed!");
+    }
+}
