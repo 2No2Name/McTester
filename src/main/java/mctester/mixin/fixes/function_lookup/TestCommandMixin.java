f21493cbc155c9f68c08279895bd1605a1a33100
2No2Name <2no2name@web.de>
2023-12-15 00:44:23 +0100

    Implement template function usage for newly created structure blocks

---
+++ src/main/java/mctester/mixin/fixes/function_lookup/TestCommandMixin.java
@@ -0,0 +1,29 @@
+package mctester.mixin.fixes.function_lookup;
+
+import net.minecraft.block.entity.StructureBlockBlockEntity;
+import net.minecraft.server.command.TestCommand;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(TestCommand.class)
+public class TestCommandMixin {
+
+    @Redirect(
+            method = "run(Lnet/minecraft/server/world/ServerWorld;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/test/TestSet;Z)V",
+            at = @At(
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/block/entity/StructureBlockBlockEntity;getMetadata()Ljava/lang/String;"
+            )
+    )
+    private static String getTestFunctionIdentifier(StructureBlockBlockEntity structureBlockBlockEntity) {
+        String retval = structureBlockBlockEntity.getMetadata();
+        if (retval.isEmpty()) {
+            retval = structureBlockBlockEntity.getTemplateName();
+            if (retval.startsWith("minecraft:")) {
+                retval = retval.substring("minecraft:".length());
+            }
+        }
+        return retval;
+    }
+}
